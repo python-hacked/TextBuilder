d68726d4ea28c0718f60d9f57393043963683576
python-hacked <satishchoudhary394@gmail.com>
2023-08-09 17:09:39 +0530

    event work

--- myapp/src/components/Textform.js
+++ myapp/src/components/Textform.js
@@ -1,14 +1,40 @@
 import React, { useState } from 'react';
 
 export default function Textform(props) {
-    const [text,] = useState("Enter Your Text");
+    const handleUpClick = () => {
+        console.log("upercase was clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+    const handleToClick = () => {
+        console.log("upercase was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    const handelOnChange = (event) => {
+        setText(event.target.value);
+    }
+    const [text, setText] = useState("");
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} id="mybox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handelOnChange} id="mybox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary">Convert To Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert To Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleToClick}>Convert To Uppercase</button>
+
         </div>
+        <div className='container my-3'>
+        <h2>Your Text Summary</h2>
+        <p>{text.split(" ").length} Words and {text.length} Charcters</p>
+        <p>{0.008*text.split(" ").length} Minutes read</p>
+        <h2>Your Text Summary</h2>
+        <p>{text}</p>
+
+
+        </div>
+        </>
     )
 }
